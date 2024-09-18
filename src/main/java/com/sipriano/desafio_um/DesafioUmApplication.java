f0bf33376c927c71f69fe2c3a6fd4d6d8c246e4a
Anderson Sipriano <Andsipriano@gmail.com>
2024-09-18 17:32:18 -0300

    first commit

---
+++ src/main/java/com/sipriano/desafio_um/DesafioUmApplication.java
@@ -0,0 +1,13 @@
+package com.sipriano.desafio_um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipriano/desafio_um/DesafioUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipriano.desafio_um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
